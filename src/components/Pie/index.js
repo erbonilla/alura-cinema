76210917f00f527eb5bad0105c82047460e51565
erbonilla <erbonilla@outlook.com>
2025-01-06 17:47:15 -0600

    Initial commit

---
+++ src/components/Pie/index.js
@@ -0,0 +1,11 @@
+import styles from "./Pie.module.css"
+import logo from "./logo-alura.png"
+
+function Pie(){
+return(<footer className={styles.pie}>
+<h2>Desarrollado por Edgar BG <img src={logo} alt="Alura"/></h2>
+
+</footer>)
+
+}
+export default Pie
